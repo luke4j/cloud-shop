9e06d4e40cddfa82eee398b84ea387e3c00fc209
luke <llg6yy@163.com>
2018-11-09 00:04:23 +0800

    hibernate 问题解决，在于hibernate-entitymanager的版本

--- server-shop/src/main/java/club/luke/cloud/shop/app/Config.java
+++ server-shop/src/main/java/club/luke/cloud/shop/app/Config.java
@@ -2,13 +2,26 @@ package club.luke.cloud.shop.app;
 
 import club.luke.cloud.shop.app.web.config.WebMvcConfigurerBean;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.engine.spi.SessionFactoryDelegatingImpl;
+import org.hibernate.internal.SessionFactoryImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 
 /**
  * Created by luke on 2018/11/5.
@@ -32,9 +45,5 @@ public class Config extends WebMvcConfigurerBean {
     public HibernateJpaSessionFactoryBean sessionFactory() {
         return new HibernateJpaSessionFactoryBean();
     }
-
-
-
-
 
 }
