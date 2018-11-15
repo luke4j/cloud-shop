e27fbc75cc5f28328e885cd4be6c82bcb9cbaee0
luke <llg6yy@163.com>
2018-11-15 10:51:37 +0800

    修改加载菜单逻辑

--- server-shop/src/main/resources/static/app/login/menu.view.js
+++ server-shop/src/main/resources/static/app/login/menu.view.js
@@ -13,28 +13,40 @@ define(function(require, exports, module) {
             this.render() ;
         },
         render:function(){
-
+            /**页面改变参数*/
             var $me = this ;
             var menus = $("<div />").addClass('container-fluid').attr("id","main_menu");
+            LukeApp.jqWorkSpace.html("") ;
+            LukeApp.jqWorkSpace.append(menus) ;
             var $groupRow ;
 
-            /**动态构建菜单*/
+            /**先处理fid为0的数据*/
             for(var i in LukeApp.info.funs){
-
                 var fun = LukeApp.info.funs[i] ;
-                if(fun.fid == 0){
-                    $groupRow = $("<div >").addClass("row") ;
+                if(fun.fid===0){
+                    $groupRow = $("<div >").addClass("row").attr("row_id",fun.id) ;
                     menus.append($groupRow) ;
                     continue ;
                 }
-                if(!fun.viewPath) continue ;
+            }
+
+            /**动态构建菜单*/
+            for(var i in LukeApp.info.funs){
+                var fun = LukeApp.info.funs[i] ;
+                if(fun.fid===0){
+                    continue ;
+                }
+                if(!fun.viewPath){
+                    console.dir("fun.id "+ fun.id +" viewPath is null") ;
+                    continue ;
+                }
                 /**加载菜单组件，为后面显示菜单做准备*/
                 var item = J.htmlTemp("app/login/menu_item.tmp.html") ;
-                item = $(item) ;
+                var $item = $(item) ;
 
-                item.attr("viewPath",fun.viewPath).attr("viewName",fun.name) ;
+                $item.attr("viewPath",fun.viewPath).attr("viewName",fun.name) ;
                 /**为每一个菜单增加事件*/
-                item.on('click',function(e){
+                $item.on('click',function(e){
                     requirejs([$(e.currentTarget).attr('viewPath')],function(VClass){
                         $("#main_menu").remove() ;
                         var viewPath = $(e.currentTarget).attr('viewPath') ;
@@ -43,16 +55,15 @@ define(function(require, exports, module) {
                     }) ;
                 }) ;
                 if(fun.iconPath){
-                    $("h3",item).html("") ;
-                    $("h3",item).append($("<p>").addClass("glyphicon").addClass(fun.iconPath)).append(fun.name) ;
+                    $("h3",$item).html("") ;
+                    $("h3",$item).append($("<p>").addClass("glyphicon").addClass(fun.iconPath)).append(fun.name) ;
                 }else{
-                    $("h3",item).text(fun.name) ;
+                    $("h3",$item).text(fun.name) ;
                 }
-                $groupRow.append(item) ;
+                $("div[row_id='"+fun.fid+"']").append($item)
             }
 
-            LukeApp.jqWorkSpace.html("") ;
-            LukeApp.jqWorkSpace.append(menus) ;
+
         }
 
     });
